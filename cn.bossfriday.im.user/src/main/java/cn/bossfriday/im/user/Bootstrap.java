d738b6b2d8ff2e2de5dc2b5d75a30af825d6e1c3
chenxiang <chenxiang@beemwork.com>
2024-10-24 18:56:22 +0800

    新建用户服务；

---
+++ cn.bossfriday.im.user/src/main/java/cn/bossfriday/im/user/Bootstrap.java
@@ -0,0 +1,47 @@
+package cn.bossfriday.im.user;
+
+import cn.bossfriday.common.bootstrap.ServicePluginBootstrap;
+import cn.bossfriday.common.conf.SystemConfigLoader;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Bootstrap
+ * <p>
+ * 用户服务
+ *
+ * @author chenx
+ */
+@Slf4j
+public class Bootstrap extends ServicePluginBootstrap {
+
+    @Override
+    protected String getServiceName() {
+        return "BossFriday-IM-User";
+    }
+
+    @Override
+    protected void start() {
+        try {
+
+        } catch (Exception ex) {
+            log.error("Bootstrap.start() error!", ex);
+        }
+    }
+
+    @Override
+    protected void stop() {
+        try {
+
+        } catch (Exception ex) {
+            log.error("Bootstrap.stop() error!", ex);
+        }
+    }
+
+    /**
+     * 本地测试启动入口
+     */
+    public static void main(String[] args) {
+        Bootstrap plugin = new Bootstrap();
+        plugin.startup(SystemConfigLoader.getInstance().getSystemConfig());
+    }
+}
